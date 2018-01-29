0aebb7501242f67a11d0e69e6b03f7f51bfebb7b
PolulyakhSofiya <polulyakh.sofiya@gmail.com>
2018-01-29 23:50:39 +0200

    Test Task Form.com

---
+++ TestTask_Form.com/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask_Form.com
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first side of the triangle");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second side of the triangle");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the third side of the triangle");
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(new Figure().TriangleType(a, b, c));
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Figure
+    {
+        public int TriangleType(int a, int b, int c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+                return 4;
+
+            if (a == b && a == c)
+                return 3;
+
+            if (a == b && a != c || a == c && a != b || b == c && b != a)
+                return 2;
+
+            return 1;
+        }
+    }
+}
---
+++ TestTestTask_Form.com/UnitTest1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestTask_Form.com;
+
+namespace TestTestTask_Form.com
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Figure figure;
+
+        [TestMethod]
+        public void TestEquilateralType()
+        {
+            this.figure = new Figure();
+
+            int a = 2;
+            int b = 2;
+            int c = 2;
+
+            Assert.AreEqual(figure.TriangleType(a, b, c), 3);
+        }
+
+        [TestMethod]
+        public void TestIsoscelesType_ABSidesAreEqual()
+        {
+            this.figure = new Figure();
+
+            int a = 2;
+            int b = 2;
+            int c = 3;
+
+            Assert.AreEqual(figure.TriangleType(a, b, c), 2);
+        }
+
+        [TestMethod]
+        public void TestIsoscelesType_ACSidesAreEqual()
+        {
+            this.figure = new Figure();
+
+            int a = 2;
+            int b = 4;
+            int c = 2;
+
+            Assert.AreEqual(figure.TriangleType(a, b, c), 2);
+        }
+
+        [TestMethod]
+        public void TestIsoscelesType_BCSidesAreEqual()
+        {
+            this.figure = new Figure();
+
+            int a = 1;
+            int b = 2;
+            int c = 2;
+
+            Assert.AreEqual(figure.TriangleType(a, b, c), 2);
+        }
+
+        [TestMethod]
+        public void TestScaleneType()
+        {
+            this.figure = new Figure();
+
+            int a = 1;
+            int b = 2;
+            int c = 3;
+
+            Assert.AreEqual(figure.TriangleType(a, b, c), 1);
+        }
+
+        [TestMethod]
+        public void TestError_AIsNegativeValue()
+        {
+            this.figure = new Figure();
+
+            int a = -1;
+            int b = 2;
+            int c = 3;
+
+            Assert.AreEqual(figure.TriangleType(a, b, c), 4);
+        }
+
+        [TestMethod]
+        public void TestError_CIsZeroValue()
+        {
+            this.figure = new Figure();
+
+            int a = 3;
+            int b = 2;
+            int c = 0;
+
+            Assert.AreEqual(figure.TriangleType(a, b, c), 4);
+        }
+    }
+}
